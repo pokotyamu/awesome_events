66e55eb5abf5bd7579c4de6cce78b3ba74a86e78
pokotyamu <pokotyamu@gmail.com>
2016-05-25 10:54:11 +0900

    userモデルにバリデーションを設定する

--- db/migrate/20160525014808_create_users.rb
+++ db/migrate/20160525014808_create_users.rb
@@ -1,12 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :provider
-      t.string :uid
-      t.string :nickname
-      t.string :image_url
+      t.string :provider,  null: false
+      t.string :uid,       null: false
+      t.string :nickname,  null: false
+      t.string :image_url, null: false
 
-      t.timestamps null: false
+      t.timestamps
     end
+
+    add_index :users, [:provider, :uid], unique:true
   end
 end
