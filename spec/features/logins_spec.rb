fe10d712f53fc742b05404cadd5950242abfedce
pokotyamu <pokotyamu@gmail.com>
2016-05-27 12:18:08 +0900

    ジェネレーターコマンドで作成したものを使うように変更

--- spec/features/logins_spec.rb
+++ spec/features/logins_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature "Logins", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ユーザがイベントの参加や登録を行うために、ログインをする' do
+    context 'トップページに遷移し、"Twitterでログイン"をクリックした時' do
+      context 'かつTwitterでログインに成功した時' do
+        before do
+          visit root_path
+          click_link 'Twitterでログイン'
+        end
+
+        it 'トップページに遷移していること' do
+          expect(page.current_path).to eq root_path
+        end
+
+        it '"ログインしました"と表示されること' do
+          expect(page).to have_content 'ログインしました'
+        end
+      end
+    end
+  end
 end
