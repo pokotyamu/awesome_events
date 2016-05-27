a9566b837c5f1f541b7b2a73e29d51799b9a7eba
pokotyamu <pokotyamu@gmail.com>
2016-05-27 15:00:41 +0900

    Event#nameが空白の状態だとvalidationエラーになるテスト(本実装していないため落ちる)

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Event, type: :model do
+  describe '#name' do
+    context '空白のとき' do
+      it 'validでないこと' do
+        event = Event.new(name: '')
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+  end
 end
