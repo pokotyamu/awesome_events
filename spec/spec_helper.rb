edf50b7b49caddd68ab157f8dc66328cdde4aaee
pokotyamu <pokotyamu@gmail.com>
2016-05-27 10:51:41 +0900

    テストで使うための仮定のmockを作る

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,18 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.before(:all, type: :feature) do
+    OmniAuth.config.test_mode = true
+    OmniAuth.counfig.mock_auth[:twitter] = OmniAuth::AuthHash.new(
+      {
+        provider: 'twitter',
+        uid: '12345',
+        info: {
+          nickname: 'netwillnet',
+          image: 'http://example.com/netwillnet.jpg'
+        }
+      })
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
