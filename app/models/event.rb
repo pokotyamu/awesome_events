5f93a29c1aad8a80ad2b27335b95ba61af5bfffa
pokotyamu <pokotyamu@gmail.com>
2016-05-30 11:18:30 +0900

    start_timeに空白に対するバリデーションを設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   validates :name, length: { maximum: 50 }, presence: true
   validates :place, length: { maximum: 100  }, presence: true
   validates :content, length: { maximum: 200 }, presence: true
+  validates :start_time, presence: true
 end
