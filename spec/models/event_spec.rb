48f946caec384f4c70d0690a18cdc7c4f88da69e
pokotyamu <pokotyamu@gmail.com>
2016-05-30 10:55:52 +0900

    nameに関して、50文字制限のテストを追加(落ちる)

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Event, type: :model do
   describe '#name' do
     it { should validate_presence_of(:name) }
+    it { should ensure_length_of(:name).is_at_most(50) }
   end
 end
