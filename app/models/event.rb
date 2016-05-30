d356906b5e89984797a3689bff1eb0fffc99f674
pokotyamu <pokotyamu@gmail.com>
2016-05-30 11:13:57 +0900

    contentに空白に対するバリデーションを設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
   validates :name, length: { maximum: 50 }, presence: true
   validates :place, length: { maximum: 100  } ,presence: true
+  validates :content, presence: true
 end
