610232a1cb6ad73e3b5e4e604179f6fb91655a63
pokotyamu <pokotyamu@gmail.com>
2016-05-30 11:10:47 +0900

    placeに対して最大100文字の制限をかける

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
   validates :name, length: { maximum: 50 }, presence: true
-  validates :place, presence: true
+  validates :place, length: { maximum: 100  } ,presence: true
 end
