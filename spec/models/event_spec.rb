91ddf82e80061bcf83c1e688d370d97aa84cd873
pokotyamu <pokotyamu@gmail.com>
2016-05-30 10:54:49 +0900

    空白の時のテストをshoulda_matcherを使って書き換える

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 describe Event, type: :model do
   describe '#name' do
-    context '空白のとき' do
-      let(:event) { Event.new(name: '') }
-
-      it 'validでないこと' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
+    it { should validate_presence_of(:name) }
   end
 end
