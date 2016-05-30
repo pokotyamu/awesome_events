e10844fd86f2731eef58b2b5de777776eac99e06
pokotyamu <pokotyamu@gmail.com>
2016-05-30 11:56:15 +0900

    start_timeがend_timeより遅い場合のバリデーションを設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,15 @@ class Event < ActiveRecord::Base
   validates :content, length: { maximum: 200 }, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
+  validate :start_time_should_be_before_end_time
+
+  private
+
+  def start_time_should_be_before_end_time
+    return unless start_time && end_time
+
+    if start_time >= end_time
+      errors.add(:start_time, 'は終了時間よりも前に設定してください')
+    end
+  end
 end
