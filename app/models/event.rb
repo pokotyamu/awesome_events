3c1f5392ee6097217fbf1c8e8e60b153670555b2
pokotyamu <pokotyamu@gmail.com>
2016-05-30 11:16:04 +0900

    contentに対して最大200文字の制限をかける

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
   validates :name, length: { maximum: 50 }, presence: true
-  validates :place, length: { maximum: 100  } ,presence: true
-  validates :content, presence: true
+  validates :place, length: { maximum: 100  }, presence: true
+  validates :content, length: { maximum: 200 }, presence: true
 end
