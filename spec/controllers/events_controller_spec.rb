6ddc6f8ed39460f743ae5970b932befe268b53fd
pokotyamu <pokotyamu@gmail.com>
2016-06-01 11:15:35 +0900

    未ログイン時にテストを作成しようとした時のテスト(落ちる)

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe EventsController, type: :controller do
+describe EventsController do
+  describe 'GET #new' do
+    context '未ログインユーザがアクセスした時' do
+      before { get :new }
 
+      it 'トップページにリダイレクトすること' do
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
 end
