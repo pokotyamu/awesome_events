7826ed726e9e9c8bbc2018243ddae9bcb0cf04c5
pokotyamu <pokotyamu@gmail.com>
2016-06-01 12:48:56 +0900

    User 用の定義をfactory_girl を用いて行った

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user do
+  factory :user, aliases: [:ownder] do
+    provider 'twitter'
+    sequence(:uid) { |i| "uid#{i}" }
+    sequence(:nickname) { |i| "nickname#{i}" }
+    sequence(:image_url) { |i| "http://example.com/image#{i}.jpg" }
   end
 end
