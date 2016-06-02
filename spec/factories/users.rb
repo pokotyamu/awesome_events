f445ca837dc09e07c3fb98d8838b4fa8b9155004
pokotyamu <pokotyamu@gmail.com>
2016-06-02 14:06:22 +0900

    typo fix

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user, aliases: [:ownder] do
+  factory :user, aliases: [:owner] do
     provider 'twitter'
     sequence(:uid) { |i| "uid#{i}" }
     sequence(:nickname) { |i| "nickname#{i}" }
