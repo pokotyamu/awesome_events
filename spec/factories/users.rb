52a0d82e2450035500c100965fa96345fe262934
pokotyamu <pokotyamu@gmail.com>
2016-06-02 14:14:43 +0900

    本の内容から今回のPRの内容だけのものに修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user, aliases: [:owner] do
+  factory :user do
     provider 'twitter'
     sequence(:uid) { |i| "uid#{i}" }
     sequence(:nickname) { |i| "nickname#{i}" }
