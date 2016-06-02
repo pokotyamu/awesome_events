78ca1c98d7f93e117a7ceef7f7abc56612f2cda0
pokotyamu <pokotyamu@gmail.com>
2016-06-02 10:48:57 +0900

    ログイン状態を取得するcurrent_user メソッドを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :logged_in?
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
 
   private
 
