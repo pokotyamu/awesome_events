8f40c2b49ce1a6084cd4ea668a0f0a4762f0d72b
pokotyamu <pokotyamu@gmail.com>
2016-06-03 11:35:29 +0900

    パラメータをもとにイベントを保存できるように実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,6 +6,14 @@ class EventsController < ApplicationController
   end
 
   def create
-    head :ok
+    @event = current_user.created_events.build(event_params)
+    @event.save
+    redirect_to @event, notice: '作成しました'
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :place, :content, :start_time, :end_time)
   end
 end
