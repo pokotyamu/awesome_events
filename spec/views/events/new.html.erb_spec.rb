0ff0c1cb9a43f36997c9c72ade7e49b65bc88625
pokotyamu <pokotyamu@gmail.com>
2016-06-03 18:44:53 +0900

    新規作成イベントページのテスト項目を書きだした(テスト内容は未実装)

--- spec/views/events/new.html.erb_spec.rb
+++ spec/views/events/new.html.erb_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "events/new", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "events/new", type: :view do
+  context 'ログインしているユーザがアクセスした時' do
+    it '"イベント作成"という見出しのページがrender されていること'
+
+    context 'かつ入力内容に間違いがあった時' do
+      it '間違っている内容が表示されていること'
+      it 'DB に保存されていないこと'
+    end
+  end
 end
