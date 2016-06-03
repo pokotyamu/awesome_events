2abff94b6ad723908814311f3866a9bcc511b59f
pokotyamu <pokotyamu@gmail.com>
2016-06-03 11:02:00 +0900

    typo fix

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     owner
     sequence(:name) { |i| "イベント名#{i}" }
     sequence(:place) { |i| "イベント開催場所#{i}" }
-    sequence(:context) { |i| "イベント本文#{i}" }
+    sequence(:content) { |i| "イベント本文#{i}" }
     start_time { rand(1..30).days.from_now }
     end_time { start_time + rand(1..30).hours }
   end
