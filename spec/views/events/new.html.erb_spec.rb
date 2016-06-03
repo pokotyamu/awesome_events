4020935a9c76612bdfe915aea27be5e9c5a7d191
pokotyamu <pokotyamu@gmail.com>
2016-06-03 18:54:01 +0900

    「イベント作成」という見出しが出ていることについてのテスト(落ちる)

--- spec/views/events/new.html.erb_spec.rb
+++ spec/views/events/new.html.erb_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 describe "events/new", type: :view do
   context 'ログインしているユーザがアクセスした時' do
-    it '"イベント作成"という見出しのページがrender されていること'
+    before do
+      assign(:event, create(:event))
+      render
+    end
+
+    it '"イベント作成"という見出しのページがrender されていること' do
+      expect(rendered).to match(/イベント作成/)
+    end
 
     context 'かつ入力内容に間違いがあった時' do
       it '間違っている内容が表示されていること'
