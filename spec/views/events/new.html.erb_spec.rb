618510a13388582f403c2f89809637222be86576
pokotyamu <pokotyamu@gmail.com>
2016-06-07 00:00:07 +0900

    間違っている内容が表示されていること、DBに保存されていないことの実装(落ちる)

--- spec/views/events/new.html.erb_spec.rb
+++ spec/views/events/new.html.erb_spec.rb
@@ -11,9 +11,21 @@ describe "events/new", type: :view do
       expect(rendered).to match(/イベント作成/)
     end
 
-    context 'かつ入力内容に間違いがあった時' do
-      it '間違っている内容が表示されていること'
-      it 'DB に保存されていないこと'
+    context 'かつ入力内容に間違い(不備)があった時' do
+      let(:error_event) { build(:event, end_time: nil) }
+      before do
+        error_event.valid?
+        assign(:event, error_event)
+        render
+      end
+
+      it '間違っている内容が表示されていること' do
+        expect(rendered).to match /#{error_event.errors.full_messages.first}/
+      end
+
+      it 'DB に保存されていないこと' do
+        expect(Event.last).not_to eq error_event
+      end
     end
   end
 end
