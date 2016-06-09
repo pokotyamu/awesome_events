bee9faef9898f39b2ca00f0c3df1ea3ff31579c5
pokotyamu <pokotyamu@gmail.com>
2016-06-09 14:19:21 +0900

    イベントの一覧が表示されているかについてのテストのみ先に実装(落ちる)

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,15 +2,24 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
   describe "GET #index" do
-    context 'ユーザがトップページにアクセスした時' do
-      context '現在時刻より前の開催時間のイベントがある時' do
-        it 'そのイベントが表示されていないこと'
+    let(:pre_event) { create_list(:fin_event, 5) }
+    let(:next_event) { create_list(:rand_event, 5) }
+    context 'イベントが複数ある状態で、ユーザがトップページにアクセスした時' do
+      before do
+        pre_event
+        next_event
+        get :index
       end
 
-      context '現在時刻より後の開催時間のイベントがある時' do
-        it 'そのイベントが一覧で表示されていること'
-        it '複数ある場合、そのイベントが開催時間が近い順でソートされていること'
+      it '現在時刻より前のイベントが表示されていないこと' do
+        expect(assigns(:events)).not_to match_array(pre_event)
       end
+
+      it 'そのイベントが一覧で表示されていること' do
+        expect(assigns(:events)).to match_array(next_event)
+      end
+
+      it '複数ある場合、そのイベントが開催時間が近い順でソートされていること'
     end
   end
 end
