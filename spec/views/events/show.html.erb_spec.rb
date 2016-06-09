7bd880fdc2e89f32b0f45b1db2564480c845ac7d
pokotyamu <pokotyamu@gmail.com>
2016-06-09 10:54:52 +0900

    show テンプレートに対するテストの修正

--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -8,19 +8,19 @@ RSpec.describe "events/show", type: :view do
       render
     end
     it 'Event の:id 番目のイベント名が表示されていること' do
-      expect(rendered).to match(/"#{assigns(:event).name}"/)
+      expect(rendered).to match(/#{event.name}/)
     end
     it 'Event の:id 番目の主催者名が表示されていること' do
-      expect(rendered).to match(/"@#{assigns(:event).nickneme}"/)
+      expect(rendered).to match(/@#{User.find(event.owner_id).nickname}/)
     end
     it 'Event の:id 番目の開催時間が表示されていること' do
-      expect(rendered).to match(/"#{assigns(:event).start_time.strftime('%Y/%m/%d %H:%M') - assigns(:event).end_time.strftime('%Y/%m/%d %H:%M') }"/)
+      expect(rendered).to match(/#{event.start_time.strftime('%Y/%m/%d %H:%M')} - #{event.end_time.strftime('%Y/%m/%d %H:%M')}/)
     end
     it 'Event の:id 番目の開催場所が表示されていること' do
-      expect(rendered).to match(/"#{assigns(:event).place}"/)
+      expect(rendered).to match(/#{event.place}/)
     end
     it 'Event の:id 番目のイベント内容が表示されていること' do
-      expect(rendered).to match(/"#{assigns(:event).content}"/)
+      expect(rendered).to match(/#{event.content}/)
     end
   end
 end
