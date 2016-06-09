31f01e457a36aee8653a4463c3210807d0cd2d3c
pokotyamu <pokotyamu@gmail.com>
2016-06-09 14:38:09 +0900

    開始時間順でソート

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @events = Event.where('start_time > ?', Time.zone.now)
+    @events = Event.where('start_time > ?', Time.zone.now).order(:start_time)
   end
 end
