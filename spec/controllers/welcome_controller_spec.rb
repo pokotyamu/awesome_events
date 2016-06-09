42981601a63a8cb6eacf80db2d199820a508f856
pokotyamu <pokotyamu@gmail.com>
2016-06-09 11:18:34 +0900

    イベント一覧に対するindex メソッドのテスト列挙

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
+  describe "GET #index" do
+    context 'ユーザがトップページにアクセスした時' do
+      context '現在時刻より前の開催時間のイベントがある時' do
+        it 'そのイベントが表示されていないこと'
+      end
 
+      context '現在時刻より後の開催時間のイベントがある時' do
+        it 'そのイベントが一覧で表示されていること'
+        it '複数ある場合、そのイベントが開催時間が近い順でソートされていること'
+      end
+    end
+  end
 end
