31d6ac3d7ff63779aba643502072f61c79c52b72
pokotyamu <pokotyamu@gmail.com>
2016-06-09 15:26:33 +0900

    テストのメッセージで:id 番目と表記しているものをx 番目に修正

--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -8,23 +8,23 @@ RSpec.describe "events/show", type: :view do
       render
     end
 
-    it 'Event の:id 番目のイベント名が表示されていること' do
+    it 'Event のx 番目のイベント名が表示されていること' do
       expect(rendered).to match(/#{event.name}/)
     end
 
-    it 'Event の:id 番目の主催者名が表示されていること' do
+    it 'Event のx 番目の主催者名が表示されていること' do
       expect(rendered).to match(/@#{User.find(event.owner_id).nickname}/)
     end
 
-    it 'Event の:id 番目の開催時間が表示されていること' do
+    it 'Event のx 番目の開催時間が表示されていること' do
       expect(rendered).to match(/#{event.start_time.strftime('%Y/%m/%d %H:%M')} - #{event.end_time.strftime('%Y/%m/%d %H:%M')}/)
     end
 
-    it 'Event の:id 番目の開催場所が表示されていること' do
+    it 'Event のx 番目の開催場所が表示されていること' do
       expect(rendered).to match(/#{event.place}/)
     end
 
-    it 'Event の:id 番目のイベント内容が表示されていること' do
+    it 'Event のx 番目のイベント内容が表示されていること' do
       expect(rendered).to match(/#{event.content}/)
     end
   end
