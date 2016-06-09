1f6ca84be44b866bb3d34062334412029cd36908
pokotyamu <pokotyamu@gmail.com>
2016-06-09 15:23:00 +0900

    イベントが単数ある時と複数の時でテストを分けた

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,26 +2,47 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
   describe "GET #index" do
-    let(:pre_event) { create_list(:closed_event, 5) }
-    let(:next_event) { create_list(:rand_event, 5) }
+    describe 'ユーザがトップページにアクセスした時' do
+      context 'イベントが単数の時' do
+        let(:pre_event) { create(:closed_event) }
+        let(:next_event) { create(:rand_event) }
 
-    context 'イベントが複数ある状態で、ユーザがトップページにアクセスした時' do
-      before do
-        pre_event
-        next_event
-        get :index
+        before do
+          pre_event
+          next_event
+          get :index
+        end
+
+        it '開催済みのイベントが表示されていないこと' do
+          expect(assigns(:events)).not_to match_array(pre_event)
+        end
+
+        it 'まだ開催されていないイベントが一覧で表示されていること' do
+          expect(assigns(:events)).to match_array(next_event)
+        end
       end
 
-      it '現在時刻より前のイベントが表示されていないこと' do
-        expect(assigns(:events)).not_to match_array(pre_event)
-      end
+      context 'イベントが複数ある時' do
+        let(:pre_events) { create_list(:closed_event, 5) }
+        let(:next_events) { create_list(:rand_event, 5) }
 
-      it 'そのイベントが一覧で表示されていること' do
-        expect(assigns(:events)).to match_array(next_event)
-      end
+        before do
+          pre_events
+          next_events
+          get :index
+        end
 
-      it '複数ある場合、そのイベントが開催時間が近い順でソートされていること' do
-        expect(assigns(:events)).to match (next_event.sort_by{ |event| event.start_time })
+        it '開催済みのイベントが表示されていないこと' do
+          expect(assigns(:events)).not_to match_array(pre_events)
+        end
+
+        it 'まだ開催されていないイベントが一覧で表示されていること' do
+          expect(assigns(:events)).to match_array(next_events)
+        end
+
+        it '表示されいてるイベントが開催時間が近い順でソートされていること' do
+          expect(assigns(:events)).to match (next_events.sort_by{ |event| event.start_time })
+        end
       end
     end
   end
