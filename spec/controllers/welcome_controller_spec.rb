d58fef4e00e124d07a06436da5f9f9ca96481bda
pokotyamu <pokotyamu@gmail.com>
2016-06-09 17:26:38 +0900

    現在と同時刻のイベントを表示しないテストを追加

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,21 +4,43 @@ RSpec.describe WelcomeController, type: :controller do
   describe "GET #index" do
     describe 'ユーザがトップページにアクセスした時' do
       context 'イベントが単数の時' do
-        let(:pre_event) { create(:closed_event) }
-        let(:next_event) { create(:future_event) }
+        context 'かつ、そのイベントが開催済みの時' do
+          let(:pre_event) { create(:closed_event) }
 
-        before do
-          pre_event
-          next_event
-          get :index
+          before do
+            pre_event
+            get :index
+          end
+
+          it 'そのイベントが表示されていないこと' do
+            expect(assigns(:events)).not_to include(pre_event)
+          end
         end
 
-        it '開催済みのイベントが表示されていないこと' do
-          expect(assigns(:events)).not_to include(pre_event)
+        context 'かつ、そのイベントが現在時刻と同じ時' do
+          let(:now_event) { create(:now_event) }
+
+          before do
+            now_event
+            get :index
+          end
+
+          it 'そのイベントが表示されていないこと' do
+            expect(assigns(:events)).not_to include(now_event)
+          end
         end
 
-        it 'まだ開催されていないイベントが一覧で表示されていること' do
-          expect(assigns(:events)).to include(next_event)
+        context 'かつ、そのイベントが開催前の時' do
+          let(:next_event) { create(:future_event) }
+
+          before do
+            next_event
+            get :index
+          end
+
+          it 'そのイベントが表示されていること' do
+            expect(assigns(:events)).to include(next_event)
+          end
         end
       end
 
--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -14,12 +14,18 @@ FactoryGirl.define do
       end_time { start_time + rand(1..30).hours }
     end
 
+    trait :now_date do
+      start_time { Time.zone.now }
+      end_time { start_time + rand(1..30).hours }
+    end
+
     trait :closed_date do
       start_time { Time.local(Time.now.year - 1, 5, 2, 12, 00, 00) }
       end_time { start_time + rand(1..30).hours }
     end
 
     factory :future_event, traits:[:seq_info, :rand_date]
+    factory :now_event, traits: [:seq_info, :now_date]
     factory :closed_event, traits:[:seq_info, :closed_date]
   end
 end
