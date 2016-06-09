ded2e6971efe31f1ab1902645423b3c9747f0194
pokotyamu <pokotyamu@gmail.com>
2016-06-09 16:24:23 +0900

    存在しないイベントへのアクセスがあった時の画面側のテスト(落ちる)

--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "events/show", type: :view do
-  context 'ユーザがアクセスした時' do
+  context '存在するページにユーザがアクセスした時' do
     let(:event) { create(:future_event) }
     before do
       assign(:event, event)
@@ -26,6 +26,20 @@ RSpec.describe "events/show", type: :view do
 
     it '選択されたEvent のイベント内容が表示されていること' do
       expect(rendered).to match(/#{event.content}/)
+    end
+  end
+
+  context '存在しないページにユーザがアクセスした時' do
+    before do
+      visit event_path(0)
+    end
+
+    it 'トップぺージがrender されていること' do
+      expect(page.current_path).to match root_path
+    end
+
+    it '"そのイベントは存在しません"とアラートが表示されること' do
+      expect(page).to have_content /そのイベントは存在しません/
     end
   end
 end
