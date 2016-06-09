222ceca577048fbe410b1c887febdf605c4b0970
pokotyamu <pokotyamu@gmail.com>
2016-06-09 10:56:09 +0900

    改行調整

--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -7,18 +7,23 @@ RSpec.describe "events/show", type: :view do
       assign(:event, event)
       render
     end
+
     it 'Event の:id 番目のイベント名が表示されていること' do
       expect(rendered).to match(/#{event.name}/)
     end
+
     it 'Event の:id 番目の主催者名が表示されていること' do
       expect(rendered).to match(/@#{User.find(event.owner_id).nickname}/)
     end
+
     it 'Event の:id 番目の開催時間が表示されていること' do
       expect(rendered).to match(/#{event.start_time.strftime('%Y/%m/%d %H:%M')} - #{event.end_time.strftime('%Y/%m/%d %H:%M')}/)
     end
+
     it 'Event の:id 番目の開催場所が表示されていること' do
       expect(rendered).to match(/#{event.place}/)
     end
+
     it 'Event の:id 番目のイベント内容が表示されていること' do
       expect(rendered).to match(/#{event.content}/)
     end
