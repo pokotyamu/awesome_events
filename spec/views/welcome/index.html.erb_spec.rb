589bdea9c21e483f239dc2b6e86e86b01bf6d2ed
pokotyamu <pokotyamu@gmail.com>
2016-06-14 23:09:07 +0900

    トップページで、ログインユーザに紐付いたイベントに対して編集ボタンを表示するテストの列挙

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "welcome/index", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'ログインユーザがアクセスした時' do
+    it 'まだ開催していないイベント一覧が表示されている'
+
+    context 'ログインユーザが作成したイベントが存在するとき' do
+      it 'そのイベントに対する「編集」ボタンが表示されている'
+    end
+  end
 end
