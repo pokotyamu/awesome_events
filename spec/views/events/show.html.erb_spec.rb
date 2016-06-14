c5f5b4320cc930962decd5d57102c8bab3715f97
pokotyamu <pokotyamu@gmail.com>
2016-06-14 10:07:23 +0900

    controller で定義しているメソッドを使うためにスタブを追加

--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -2,11 +2,21 @@ require "rails_helper"
 
 RSpec.describe "events/show", type: :view do
   context '存在するページにユーザがアクセスした時' do
+    before do
+      def view.logged_in?
+      end
+
+      def view.current_user
+      end
+    end
+
     context '選択されたEvent の主催者がログインユーザの時' do
       let(:user) { create(:user) }
       let(:user_event) { create(:future_event, owner_id: user.id) }
 
       before do
+        allow(view).to receive(:logged_in?) { true }
+        allow(view).to receive(:current_user) { user }
         session[:user_id] = user.id
         assign(:event, user_event)
         render
@@ -43,13 +53,14 @@ RSpec.describe "events/show", type: :view do
       let(:other_event) { create(:future_event, owner_id: other_user.id) }
 
       before do
-        session[:user_id] = other_user.id
+        allow(view).to receive(:logged_in?) { false }
+        allow(view).to receive(:current_user) { nil }
         assign(:event, other_event)
         render
       end
 
       it '選択されたEvent のイベント名が表示されていること' do
-        expect(rendered).to match(/#{ohter_event.name}/)
+        expect(rendered).to match(/#{other_event.name}/)
       end
 
       it '選択されたEvent の主催者名が表示されていること' do
