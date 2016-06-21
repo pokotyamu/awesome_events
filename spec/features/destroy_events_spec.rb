e62417b18230afad1af5c91335ea5a7817dad99c
pokotyamu <pokotyamu@gmail.com>
2016-06-21 09:23:15 +0900

    イベント削除に対するE2E テスト(列挙)

--- spec/features/destroy_events_spec.rb
+++ spec/features/destroy_events_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.feature "DestroyEvents", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ユーザがイベントの削除を行う時' do
+    context 'かつ、イベント一覧ページから行う時' do
+      context 'かつ、削除確認で「ok」を押した時' do
+        it 'イベント一覧ページが表示されていること'
+        it '"削除しました"と表示されてること'
+      end
+
+      context 'かつ、削除確認で「キャンセル」を押した時' do
+        it 'イベント一覧ページが表示されていること'
+      end
+    end
+    context 'かつ、イベント詳細ページから行う時' do
+      context 'かつ、削除確認で「ok」を押した時' do
+        it 'イベント一覧ページが表示されていること'
+        it '"削除しました"と表示されてること'
+      end
+
+      context 'かつ、削除確認で「キャンセル」を押した時' do
+        it 'そのイベントの詳細ページが表示されていること'
+      end
+    end
+  end
 end
