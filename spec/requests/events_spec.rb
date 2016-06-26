beeabdca69b84516b07212c95e846206bb72993b
pokotyamu <pokotyamu@gmail.com>
2016-06-26 10:25:05 +0900

    その場で作成する必要ないので、 let を使う形に変更

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "Events", type: :request do
   describe 'GET /events/:id' do
     context '存在するイベントにアクセスした時' do
-      let!(:event) { create(:future_event) }
-      let!(:template) { 'show' }
+      let(:event) { create(:future_event) }
+      let(:template) { 'show' }
       subject { get "/events/#{event.id}"}
 
       it_behaves_like 'HTTP 200 OK'
@@ -12,7 +12,7 @@ RSpec.describe "Events", type: :request do
     end
 
     context '存在しないイベントにアクセスした時' do
-      let!(:redirect_path) { root_path }
+      let(:redirect_path) { root_path }
       subject { get "/events/100"}
       it_behaves_like 'HTTP 302 OK'
       it_behaves_like 'redirect'
