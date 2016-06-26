b637ec194d0d4f3cdc062f3267554f91ad44664d
pokotyamu <pokotyamu@gmail.com>
2016-06-26 15:33:14 +0900

    `welcome#index` のリクエスト関係のテストを作成

--- spec/requests/welcomes_spec.rb
+++ spec/requests/welcomes_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Welcomes", type: :request do
-  describe "GET /welcomes" do
-    it "works! (now write some real specs)" do
-      get welcomes_path
-      expect(response).to have_http_status(200)
-    end
+  describe "GET /" do
+    subject { get '/' }
+
+    let(:template) { 'index' }
+
+    it_behaves_like 'HTTP 200 OK'
+    it_behaves_like 'render template'
   end
 end
