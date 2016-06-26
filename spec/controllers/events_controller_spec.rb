c9f60f05a97785c94c8d467a65a43e5f05b76862
pokotyamu <pokotyamu@gmail.com>
2016-06-26 12:35:35 +0900

    `event#new` の今ままでリクエストをテストしていた部分を削除

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 
 describe EventsController do
   describe 'GET #new' do
-    context '未ログインユーザがアクセスした時' do
-      before { get :new }
-
-      it 'トップページにリダイレクトすること' do
-        expect(response).to redirect_to(root_path)
-      end
-    end
-
     context 'ログインユーザがアクセスした時' do
       before do
         user = create(:user)
@@ -17,16 +9,8 @@ describe EventsController do
         get :new
       end
 
-      it 'ステータスコードとして200が返ること' do
-        expect(response.status).to eq(200)
-      end
-
       it '@event に、新規Event オブジェクトが格納されていること' do
         expect(assigns(:event)).to be_a_new(Event)
-      end
-
-      it 'new テンプレートをrender していること' do
-        expect(response).to render_template :new
       end
     end
   end
