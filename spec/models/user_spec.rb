39f4bde43922af76d47f5499001da81c597d750f
pokotyamu <pokotyamu@gmail.com>
2016-06-28 22:26:58 +0900

    User モデルのテスト
    * テスト作成忘れだが気づいたのでこのタイミングで追加した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe 'バリデーション' do
+    context 'provider' do
+      context 'nil の時' do
+        it 'バリデーションエラーとなること' do
+          user.provider = nil
+          expect(user).to be_valid
+        end
+      end
+    end
+
+    context 'uid' do
+      context 'nil の時' do
+        it 'バリデーションエラーとなること' do
+          user.uid = nil
+          expect(user).to be_valid
+        end
+      end
+    end
+
+    context 'nickname' do
+      context 'nil の時' do
+        it 'バリデーションエラーとなること' do
+          user.nickname = nil
+          expect(user).to be_valid
+        end
+      end
+    end
+
+    context 'image_url' do
+      context 'nil の時' do
+        it 'バリデーションエラーとなること' do
+          user.image_url = nil
+          expect(user).to be_valid
+        end
+      end
+    end
+  end
+
+  describe 'リレーション' do
+    it { should have_many(:created_events) }
+  end
 end
