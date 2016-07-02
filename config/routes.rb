6d7d6334c45a3633dee5a04267cfc7f72d459d8c
pokotyamu <pokotyamu@gmail.com>
2016-07-02 15:54:17 +0900

    `event` の子リソースとして`ticket`のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :events
+  resources :events do
+    resources :tickets
+  end
   root to: 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
